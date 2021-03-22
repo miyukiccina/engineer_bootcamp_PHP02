682f3920cd3af53a777e2b28973f1fe7cf1ce39c
miyukiccina <80190927+miyukiccina@users.noreply.github.com>
2021-03-22 19:22:12 +0900

    first commit

---
+++ sample_php/post/form_post1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>フォーム：POST</title>
+</head>
+<body>
+
+<form method="post" action="form_post2.php">
+<p>お名前:<input type="text" name="name" size="20"></p>
+<p>MAIL:<input type="text" name="mail" size="20"></p>
+
+<!-- 演習４ -->
+<p>電話番号<input type="tel" name="tel" size="20"></p>
+<p>入力日<input type="date" name="date" size="20"></p>
+<p>パスワード<input type="password" name="password" size="20"></p>
+
+<p><input type="submit" value="送信"></p>
+</form>
+
+</body>
+</html>
---
+++ sample_php/post/form_post2.php
@@ -0,0 +1,19 @@
+<?php
+    // input nameを受け取って表示する
+    $name = $_POST["name"];
+    echo $name."<br>";
+
+    $mail = $_POST["mail"];
+    echo $mail."<br>";
+
+    $tel = $_POST["tel"];
+    echo $tel."<br>";
+
+    $date = $_POST["date"];
+    echo $date."<br>";
+
+    $password = $_POST["password"];
+    echo $password."<br>";
+
+
+?>
